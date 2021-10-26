c4f38b67e0dc38d13d6eaa968341a5f92c90072f
Harsh Sonkusare <sonkus_921922@student.nitw.ac.in>
2021-10-26 23:21:12 +0530

    imag-editor js

--- src/constants/toolsForEditor.js
+++ src/constants/toolsForEditor.js
@@ -1,5 +1,6 @@
+import ImageTool from '@editorjs/image';
+import { API } from './extras';
 const Header = require('@editorjs/header');
-const SimpleImage = require("@editorjs/simple-image");
 const List = require("@editorjs/list")
 const Checklist = require("@editorjs/checklist");
 const Quote = require("@editorjs/quote");
@@ -28,9 +29,84 @@ export const tools = {
     /**
      * Or pass class directly without any configuration
      */
+    // image: {
+    //     class: ImageTool,
+    //     config: {
+    //         endpoints: {
+    //         byFile: `${API}/uploadImg`, // Your backend file uploader endpoint
+    //         byUrl: `${API}/fetchUrl`, // Your endpoint that provides uploading by Url
+    //         }
+    //     },
+    //     inlineToolbar: true,
+    // },
+
     image: {
-        class: SimpleImage,
-        inlineToolbar: true,
+      class: ImageTool,
+      config: {
+        /**
+         * Custom uploader
+         */
+        uploader: {
+          /**
+           * Upload file to the server and return an uploaded image data
+           * @param {File} file - file selected from the device or pasted by drag-n-drop
+           * @return {Promise.<{success, file: {url}}>}
+           */
+          async uploadByFile(file) {
+            console.log(file);
+            const requestOptions = {
+              method: "POST",
+              headers: {
+                
+              },
+              body: JSON.stringify(file),
+            };
+            const response = await fetch(
+              `${API}/uploadImg`,
+              requestOptions
+            );
+            let downloadURL = ""
+            if(!response){
+              return {
+                success: 0,
+                file: {
+                    url: downloadURL
+                }
+              }
+            }
+            const data = await response.json();
+            if(data.err){
+              return {
+                success: 0,
+                file: {
+                    url: downloadURL
+                }
+              }
+            }
+            downloadURL = data.file_path;
+            return {
+                success: 1,
+                file: {
+                    url: downloadURL
+                }
+            }
+          },
+
+          /**
+           * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
+           * @param {string} url - pasted image URL
+           * @return {Promise.<{success, file: {url}}>}
+           */
+           async uploadByUrl(url){
+            return {
+                success: 1,
+                file: {
+                    url: url
+              }
+            }
+          }
+        }
+      }
     },
 
     list: {
